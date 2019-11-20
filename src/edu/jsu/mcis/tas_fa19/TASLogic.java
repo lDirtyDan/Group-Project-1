979da04839a19eae79687b7e881844bbda88b70c
Richard <richastrick1998@gmail.com>
2019-11-20 11:19:52 -0600

    Feature 4 Skeleton Created

--- src/edu/jsu/mcis/tas_fa19/TASLogic.java
+++ src/edu/jsu/mcis/tas_fa19/TASLogic.java
@@ -1,6 +1,18 @@
 package edu.jsu.mcis.tas_fa19;
 
+import java.util.ArrayList;
+
 
 public class TASLogic {
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+        punchManagment(dailypunchlist);
     
+        return 0; //replace later
+    }
+    
+    public static void punchManagment(ArrayList<Punch> dailypunchlist){
+        for(int list=0; list>0; list++){
+        
+        }
+    }
 }
